3b99241ea67bb3bcbd2f8a79bd007e7bbbca2851
K-Sato1995 <katsuki_sato0130@icloud.com>
2022-10-08 14:15:25 +0900

    Fix stuff

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
-import React, { useState, useRef } from "react";
-import CustomeImage from "./components/CustomeImage";
-import {
-  exportComponentAsJPEG,
-  exportComponentAsPNG,
-} from "react-component-export-image";
-import "./App.css";
+import React, { useState, useRef } from 'react'
+import CustomeImage from './components/CustomeImage'
+import { exportComponentAsJPEG, exportComponentAsPNG } from 'react-component-export-image'
+import './App.css'
 
-function App() {
-  const componentRef = useRef();
-  const [file, setFile] = useState();
+function App () {
+  const componentRef = useRef()
+  const [file, setFile] = useState()
 
   const handleChange = (e) => {
-    console.log(e.target.files);
-    setFile(URL.createObjectURL(e.target.files[0]));
-  };
+    console.log(e.target.files)
+    setFile(URL.createObjectURL(e.target.files[0]))
+  }
 
   return (
-    <div className="App">
-      <button onClick={() => exportComponentAsJPEG(componentRef)}>
-        JPEGでエクスポート
-      </button>
-      <button onClick={() => exportComponentAsPNG(componentRef)}>
-        PNGでエクスポート
-      </button>
+      <div className="App">
+          <button onClick={() => exportComponentAsJPEG(componentRef)}>JPEGでエクスポート</button>
+          <button onClick={() => exportComponentAsPNG(componentRef)}>PNGでエクスポート</button>
 
-      <h2>Add Image:</h2>
-      <input type="file" onChange={handleChange} />
-      <CustomeImage file={file} ref={componentRef} />
-    </div>
-  );
+          <h2>Add Image:</h2>
+          <input type="file" onChange={handleChange} />
+          <CustomeImage file={file} ref={componentRef} />
+      </div>
+
+  )
 }
 
-export default App;
+export default App
--- src/components/CustomeImage.jsx
+++ src/components/CustomeImage.jsx
@@ -1,17 +1,22 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
+import styles from './custome-image.css'
 
 const CustomeImage = React.forwardRef((props, ref) => {
-  const { file } = props;
+  const { file } = props
   const [playerInfo, setPlayerInfo] = useState({
-    name: "",
-    uniformNumber: 0,
-  });
+    name: 'sss',
+    uniformNumber: 0
+  })
 
   return (
-    <div>
+    <div className={styles.container}>
+      <div>{playerInfo.name}</div>
+      <button className="custome_image_container__btn" onClick={() => { setPlayerInfo('test') }}>Click</button>
       <img src={file} ref={ref} alt="player" />
     </div>
-  );
-});
+  )
+})
 
-export default CustomeImage;
+CustomeImage.displayName = 'CustomeImage'
+
+export default CustomeImage
